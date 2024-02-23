f6288bf49247a713fb24df3406fa6520355a14dc
jairock007 <73494601+jairock007@users.noreply.github.com>
2024-02-24 01:15:45 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  cors: "",
+  cors: "https://tic-tac-toe-multiplayer-game-front.vercel.app/",
 });
 
 const allUsers = {};
